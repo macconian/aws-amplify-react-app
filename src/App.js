e94d5d2dd8da049aefed882a387434057611ec7a
Daniel Macarrón <daniel.macarron@tecsidel.com>
2020-12-16 11:25:36 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          REACT APP
+          REACT APP TEST VERSION
         </p>
         <a
           className="App-link"
